4bbb9500f4fab9b1f141beff80d2f5106f9d0067
Renata Happle <renatahapp@gmail.com>
2018-07-30 21:15:56 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,16 @@
-//add your code here
+$(document).ready(function(){ 
+  doMath();
+  }); 
+  var num1 = $('#number1'); 
+  var num2 = $('#number2'); 
+  var operator = $('#operation'); 
+  function doMath(){ 
+    $('#equals').on('click', function(){ 
+     num1 = parseFloat(num1.val()); 
+     num2 = parseFloat(num2.val()); 
+     operator = operator.val(); 
+     
+     result(num1, num2, operator)
+      
+    })
+  }
